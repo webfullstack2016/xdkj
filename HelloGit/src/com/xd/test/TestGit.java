88398a89db1560fba1f15315f94de96b3d7047aa
webfullstack2016 <51957183+webfullstack2016@users.noreply.github.com>
2019-06-19 14:30:53 +0800

    Update TestGit.java

--- HelloGit/src/com/xd/test/TestGit.java
+++ HelloGit/src/com/xd/test/TestGit.java
@@ -3,7 +3,8 @@ package com.xd.test;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("git First�ύ");
+		System.out.println("git FirstÌá½»");
+                System.out.println("git server update");
 
 	}
 
